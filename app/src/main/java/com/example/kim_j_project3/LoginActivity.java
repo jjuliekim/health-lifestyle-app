82205ca028d96f3254e321b12ef2fbb4ad83fc94
Julie Kim <118621382+jjuliekim@users.noreply.github.com>
2024-05-22 21:17:19 -0400

    log in

--- app/src/main/java/com/example/kim_j_project3/LoginActivity.java
+++ app/src/main/java/com/example/kim_j_project3/LoginActivity.java
@@ -1,7 +1,11 @@
 package com.example.kim_j_project3;
 
+import android.content.Intent;
+import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.EditText;
+import android.widget.Toast;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -25,11 +29,48 @@ public class LoginActivity extends AppCompatActivity {
 
     // handles process when log in button is clicked
     public void loggingIn(View view) {
+        EditText usernameText = findViewById(R.id.userText);
+        String username = usernameText.getText().toString();
+        EditText passwordText = findViewById(R.id.pwText);
+        String password = passwordText.getText().toString();
 
+        // check if entries are empty
+        if (username.isEmpty() || password.isEmpty()) {
+            Toast.makeText(this, "Invalid Inputs", Toast.LENGTH_SHORT).show();
+            usernameText.setText("");
+            passwordText.setText("");
+            return;
+        }
+
+        // check if username already exists
+        SharedPreferences sharedPreferences = getSharedPreferences("UserData", MODE_PRIVATE);
+        Intent myIntent = new Intent(LoginActivity.this, DashboardActivity.class);
+        if (sharedPreferences.contains(username)) {
+            // validate password
+            String storedPw = sharedPreferences.getString(username + "_password", null);
+            if (password.equals(storedPw)) {  // correct pw
+                Toast.makeText(this, "Logging In", Toast.LENGTH_SHORT).show();
+                myIntent.putExtra("username", username);
+                startActivity(myIntent);
+            } else {  // incorrect pw
+                Toast.makeText(this, "Incorrect Password", Toast.LENGTH_SHORT).show();
+                usernameText.setText("");
+                passwordText.setText("");
+            }
+        } else {
+            Toast.makeText(this, "User Does Not Exist", Toast.LENGTH_SHORT).show();
+        }
     }
 
-    // handles process when sign up button is clicked
+    // go to sign up page
     public void signingUp(View view) {
-
+        EditText usernameText = findViewById(R.id.userText);
+        String username = usernameText.getText().toString();
+        if (username.isEmpty()) {
+            username = "";
+        }
+        Intent myIntent = new Intent(LoginActivity.this, SignupActivity.class);
+        myIntent.putExtra("username", username);
+        startActivity(myIntent);
     }
 }
