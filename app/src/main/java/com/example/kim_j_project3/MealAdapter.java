749dd2f456debe4c011184916d01dd9a10182fee
Julie Kim <118621382+jjuliekim@users.noreply.github.com>
2024-05-24 16:18:43 -0400

    meal item xml

--- app/src/main/java/com/example/kim_j_project3/MealAdapter.java
+++ app/src/main/java/com/example/kim_j_project3/MealAdapter.java
@@ -5,6 +5,7 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ArrayAdapter;
+import android.widget.TextView;
 
 import java.util.List;
 
@@ -17,7 +18,12 @@ public class MealAdapter extends ArrayAdapter<Meal> {
     public View getView(int position, View convertView, ViewGroup parent) {
         Meal meal = getItem(position);
         convertView = LayoutInflater.from(getContext()).inflate(R.layout.meal_item, parent, false);
-        // set name and cal values on xml textviews
+
+        TextView nameText = convertView.findViewById(R.id.meal_name);
+        TextView amtText = convertView.findViewById(R.id.meal_cal);
+        nameText.setText(meal.getName());
+        amtText.setText(meal.getCalories());
+
         return convertView;
     }
 }
