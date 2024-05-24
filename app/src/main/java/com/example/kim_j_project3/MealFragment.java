88e5816c342f19e9d6fd7735a4610806a5268e52
Julie Kim <118621382+jjuliekim@users.noreply.github.com>
2024-05-24 17:08:47 -0400

    add meal dialog

--- app/src/main/java/com/example/kim_j_project3/MealFragment.java
+++ app/src/main/java/com/example/kim_j_project3/MealFragment.java
@@ -1,7 +1,10 @@
 package com.example.kim_j_project3;
 
 import static com.example.kim_j_project3.JsonManager.loadMeals;
+import static com.example.kim_j_project3.JsonManager.saveMeals;
 
+import android.app.AlertDialog;
+import android.content.DialogInterface;
 import android.os.Bundle;
 
 import androidx.fragment.app.Fragment;
@@ -11,12 +14,16 @@ import androidx.recyclerview.widget.RecyclerView;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
 
+import java.util.ArrayList;
 import java.util.List;
 
 public class MealFragment extends Fragment {
     private String username;
-    private List<Meal> mealList;
+    private ArrayList<Meal> mealList;
     public MealFragment() {}
 
     public static MealFragment newInstance(String username) {
@@ -34,13 +41,65 @@ public class MealFragment extends Fragment {
     }
 
     @Override
-    public View onCreateView(LayoutInflater inflater, ViewGroup container,
-                             Bundle savedInstanceState) {
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
         View view = inflater.inflate(R.layout.fragment_meal, container, false);
         mealList = loadMeals(getContext(), username);
         RecyclerView recyclerView = view.findViewById(R.id.meal_recycler_view);
         recyclerView.setLayoutManager(new LinearLayoutManager(getContext()));
         recyclerView.setAdapter(new MealAdapter(mealList));
         return view;
+    }
+
+    // show dialog for adding meal
+    public void addMealDialog(View view) {
+        AlertDialog.Builder builder = new AlertDialog.Builder(getContext());
+        LayoutInflater inflater = requireActivity().getLayoutInflater();
+        View dialogView = inflater.inflate(R.layout.dialog_add_meal, null);
+        builder.setView(dialogView);
+
+        EditText mealNameText = dialogView.findViewById(R.id.add_meal_name);
+        EditText mealCalText = dialogView.findViewById(R.id.add_meal_calories);
+
+        builder.setTitle("Add Meal")
+                .setPositiveButton("Add", null)
+                .setNegativeButton("Cancel", new DialogInterface.OnClickListener() {
+                    public void onClick(DialogInterface dialog, int id) {
+                        dialog.cancel();
+                    }
+                });
+
+        AlertDialog dialog = builder.create();
+        dialog.setOnShowListener(new DialogInterface.OnShowListener() {
+            @Override
+            public void onShow(DialogInterface dialogInterface) {
+                Button buttonAdd = dialog.getButton(AlertDialog.BUTTON_POSITIVE);
+                buttonAdd.setOnClickListener(new View.OnClickListener() {
+                    @Override
+                    public void onClick(View view) {
+                        String mealName = mealNameText.getText().toString();
+                        String mealCaloriesStr = mealCalText.getText().toString();
+
+                        if (mealName.isEmpty() || mealCaloriesStr.isEmpty()) {
+                            Toast.makeText(getContext(), "Invalid Input", Toast.LENGTH_SHORT).show();
+                            return;
+                        }
+
+                        int mealCalories;
+                        try {
+                            mealCalories = Integer.parseInt(mealCaloriesStr);
+                        } catch (NumberFormatException e) {
+                            Toast.makeText(getContext(), "Invalid Numerical Input", Toast.LENGTH_SHORT).show();
+                            return;
+                        }
+                        Meal newMeal = new Meal(mealName, mealCalories);
+                        mealList.add(newMeal);
+
+                        saveMeals(getContext(), mealList, username);
+                        dialog.dismiss();
+                    }
+                });
+            }
+        });
+        dialog.show();
     }
 }
