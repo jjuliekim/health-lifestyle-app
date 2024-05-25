b69668f615fa4db8197fc17cd7b029627d3bfb36
Julie Kim <118621382+jjuliekim@users.noreply.github.com>
2024-05-25 01:30:15 -0400

    summary user stats

--- app/src/main/java/com/example/kim_j_project3/SummaryFragment.java
+++ app/src/main/java/com/example/kim_j_project3/SummaryFragment.java
@@ -1,14 +1,19 @@
 package com.example.kim_j_project3;
 
+import android.content.Context;
+import android.content.SharedPreferences;
 import android.os.Bundle;
 
 import androidx.fragment.app.Fragment;
 
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.TextView;
 
 public class SummaryFragment extends Fragment {
+    private String username;
 
     public SummaryFragment() {
     }
@@ -24,6 +29,30 @@ public class SummaryFragment extends Fragment {
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
-        return inflater.inflate(R.layout.fragment_summary, container, false);
+        View view = inflater.inflate(R.layout.fragment_summary, container, false);
+        if (getArguments() != null) {
+            username = getArguments().getString("username");
+        }
+
+        setUserInfo(view);
+
+        return view;
     }
+
+    private void setUserInfo(View view) {
+        // set user info texts
+        TextView heightText = view.findViewById(R.id.heightTextView);
+        TextView weightText = view.findViewById(R.id.weightTextView);
+        TextView bmiText = view.findViewById(R.id.bmiTextView);
+
+        SharedPreferences sharedPreferences = getContext().getSharedPreferences("UserData", Context.MODE_PRIVATE);
+        String height = sharedPreferences.getString(username + "_height", "");
+        String weight = sharedPreferences.getString(username + "_weight", "");
+        double bmi = (Double.parseDouble(weight) * 703) / Math.pow(Double.parseDouble(height), 2);
+
+        heightText.setText(String.format("Height: %s", height));
+        weightText.setText(String.format("Weight: %s", weight));
+        bmiText.setText(String.format("BMI: %.2f", bmi));
+    }
+
 }
