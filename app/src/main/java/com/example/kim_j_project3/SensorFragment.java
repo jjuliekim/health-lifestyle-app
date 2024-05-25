4a44c67e9fa44c9a5ed3a7881e2a46535d9fe8e9
Julie Kim <118621382+jjuliekim@users.noreply.github.com>
2024-05-25 15:27:49 -0400

    heart rate sensor

--- app/src/main/java/com/example/kim_j_project3/SensorFragment.java
+++ app/src/main/java/com/example/kim_j_project3/SensorFragment.java
@@ -1,5 +1,8 @@
 package com.example.kim_j_project3;
 
+import android.content.Context;
+import android.hardware.Sensor;
+import android.hardware.SensorManager;
 import android.os.Bundle;
 
 import androidx.fragment.app.Fragment;
@@ -7,8 +10,12 @@ import androidx.fragment.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.TextView;
 
 public class SensorFragment extends Fragment {
+    private TextView heartRateText;
+    private Sensor heartRateSensor;
+    private SensorManager sensorManager;
 
     public SensorFragment() {
     }
@@ -20,10 +27,14 @@ public class SensorFragment extends Fragment {
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        sensorManager = (SensorManager) getActivity().getSystemService(Context.SENSOR_SERVICE);
+        heartRateSensor = sensorManager.getDefaultSensor(Sensor.TYPE_HEART_RATE);
     }
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
-        return inflater.inflate(R.layout.fragment_sensor, container, false);
+        View view = inflater.inflate(R.layout.fragment_sensor, container, false);
+        heartRateText = view.findViewById(R.id.heartRateTextView);
+        return view;
     }
 }
